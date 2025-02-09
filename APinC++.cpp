822b9314b9ab9a8f1faca337599dcd632378d2f7
Kirat Anand <167013352+kirat-anand-68@users.noreply.github.com>
2025-02-09 16:41:46 +0530

    Create APinC++.cpp

---
+++ APinC++.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int i;
+    for(i=100;i>=0;i-=3){
+      
+        cout<<i<<'\n';
+    }
+}
